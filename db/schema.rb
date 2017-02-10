d75b6cd6c7b93305741d24edf7094b3cd0df4b52
Christle Lee <christle.lee86@gmail.com>
2017-02-10 14:22:46 +1100

    i love myself

--- db/schema.rb
+++ db/schema.rb
@@ -11,49 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209234454) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "books", force: :cascade do |t|
-    t.integer  "isbn"
-    t.string   "title"
-    t.string   "author"
-    t.string   "publisher"
-    t.date     "date_published"
-    t.text     "description"
-    t.string   "img_url"
-    t.string   "status"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  create_table "requests", force: :cascade do |t|
-    t.integer  "user_id"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "reservations", force: :cascade do |t|
-    t.integer  "book_id"
-    t.integer  "user_id"
-    t.date     "date_requested"
-    t.date     "date_borrowed"
-    t.date     "date_due"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.boolean  "admin",           default: false
-    t.string   "class"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-  end
 
 end
