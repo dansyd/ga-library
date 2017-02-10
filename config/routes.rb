6988669bc04c6ae26765a0bd97edc8a8227474d3
dting0104 <dting@outlook.com>
2017-02-10 11:44:47 +1100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  root :to 'pages#index'
+  resources :users
+  resources :books
+  resources :reservations
+  resources :requests
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 
 end
