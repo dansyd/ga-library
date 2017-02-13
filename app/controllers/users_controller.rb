a9cbc56f3738e2809c1197120c32daccbafaffa7
Daniele Peviani <daniele.peviani@gmail.com>
2017-02-13 17:45:46 +1100

    Fixed book delivery and added waitlist in user and admin dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
 
   def dashboard
     @reserved_books = Book.where({status: 'reserved'})
+    @waitlist = PendingItem.all
+    @student_waitlist = PendingItem.where({user_id: @current_user.id})
     @requests = Request.all.map do |r|
       {info: $gr_client.search_books(r.isbn).results.work.best_book, id: r.id, isbn: r.isbn}
     end
@@ -58,6 +60,7 @@ class UsersController < ApplicationController
 
   def deliver
     Book.find(params[:id]).update({status: 'borrowed'})
+    Book.find(params[:id]).reservation.where({date_borrowed: nil}).first.update({date_borrowed: Date.today.to_s, date_due: (Date.today +10 ).to_s})
     redirect_to :back
   end
 
