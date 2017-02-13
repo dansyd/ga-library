208d29dfadc429b47c82f1bdcb927f5202b22a60
Daniele Peviani <daniele.peviani@gmail.com>
2017-02-13 22:17:55 +1100

    Added notifications on navbar and slider on homepage

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
-$('.dropdown').click(function (e) {
-    // e.preventDefault();
-    e.stopPropagation();
-
-  $('.nav-dropdown').toggle();
-});
-
-$('html').click(function(){
-  $('.nav-dropdown').hide();
-});
-
-$("#nav-toggle").click(function() {
-      $("nav ul").toggle();
-    });
-    // Hamburger to X toggle
-    document.querySelector("#nav-toggle").addEventListener("click", function() {
-      this.classList.toggle("active");
-    });
-
+  $('.dropdown').click(function (e) {
+      // e.preventDefault();
+      e.stopPropagation();
+    $('.nav-dropdown').toggle();
   });
+
+  $('html').click(function(){
+    $('.nav-dropdown').hide();
+  });
+
+  $("#nav-toggle").click(function() {
+    $("nav ul").toggle();
+  });
+  // Hamburger to X toggle
+  document.querySelector("#nav-toggle").addEventListener("click", function() {
+    this.classList.toggle("active");
+  });
+
+  $('.slider').slick({});
+
+});
