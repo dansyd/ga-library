30f0f214ca533725e070c703299855113f4d2c84
Daniele Peviani <daniele.peviani@gmail.com>
2017-02-16 17:33:13 +1100

    changed migration

--- db/migrate/20170209234408_create_users.rb
+++ db/migrate/20170209234408_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.boolean :admin, default: false
-      t.string :course
       t.string :email
       t.string :password_digest
       t.timestamps null: false
